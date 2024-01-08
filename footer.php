4aaca24b27c0e5041ff4f2d8515cd85c1e9fc827
smirtiuniyal <155731644+smirtiuniyal@users.noreply.github.com>
2024-01-08 17:49:54 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+<link rel="shortcut icon" href="fav.ico" />
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link href="css/style-1.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&family=Poppins&display=swap" rel="stylesheet"> 
 </head>
 <body>
 <footer>
