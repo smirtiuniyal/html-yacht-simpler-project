b7af182c037e37a349eb8f502920f09b06c34b84
smirtiuniyal <155731644+smirtiuniyal@users.noreply.github.com>
2024-01-08 17:41:04 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer>
+		<div class="footer_top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3">
+						<article>
+							<figure><img src="images/footer-l.png"></figure>
+						</article>
+					</div>
+					<div class="col-lg-6">
+						<article class="control_margin">
+							<h3>quick links</h3>
+							<ul class="clearfix custom_ul">
+								<li><a href="#">Layout</a></li>
+								<li><a href="#">Staterooms</a></li>
+								<li><a href="#">Activities</a></li>
+								<li><a href="#">Photo Gallery</a></li>     
+								<li><a href="#">Places & Pricing </a></li>
+							</ul>
+						</article>
+					</div>
+					<div class="col-lg-3">
+						<article class="address_footer">
+							<h3>contact us</h3>
+							<ul class="clearfix">
+								<li>Phone :<a href="tel:9723452003"> (972) 345-2003</a></li>
+								<li>Email :<a href="mailto:tkyle@yachtSimpler.com"> TKyle@YachtSimpler.com</a></li>
+							</ul>
+							<div class="media"> 
+								<a href="#"><img src="images/facebook.png"></a> 
+								<a href="#"><img src="images/twitter.png"></a> 
+								<a href="#"><img src="images/youtube.png"></a> 								
+								<a href="#"><img src="images/instagram.png"></a> 								
+							</div>
+						</article>
+					</div>
+				</div>
+			</div>
+			<!---container--> 
+		</div>
+		<!--footer_top-->
+    
+		<div class="container">
+			<div class="footer_bottom">		
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="footer_left">© 2021 YachtSimpler.com. <span>All Rights Reserved.</span></div>
+					</div>
+					<div class="col-sm-6">
+						<div class="footer_right">Powered By <a href="" target="_blank">MIDAS</a> </div>
+					</div>
+				</div>
+			</div>
+			<!-- /container --> 
+		</div>
+	</footer>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header>
+		<div class="container clearfix">
+			<div class="header_right clearfix">
+				<div class="top_link clearfix"> <span class="nav_icon"><i class="fa fa-bars"></i></span>
+				<!--top_link--> 
+			</div>
+			<div class="header_left clearfix"> <a class="site_logo" href="index.html"><img src="images/logo.png" /></a></div>
+			<div class="nav_link"> 
+				<span class="nav_close"><i class="fa fa-times"></i></span>
+				<ul class="clearfix">
+					<li><a href="layout.html">Layout</a></li>
+					<li><a href="staterooms.html">staterooms</a></li>
+					<li><a href="activities.html">activities</a></li>
+					<li><a href="photo-gallery.html">photo gallery</a></li>
+					<li><a href="place_pricing.html">places & pricing</a></li>
+				</ul>
+			</div>
+			<!--nav_link-->       
+		</div>
+        </div>
+	</header>
+</body>
+</html>
